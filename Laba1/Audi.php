7c6320d9b21855c02d1cfcb7a80dcc07f525c900
Partizanchik <60707856+Partizanchik@users.noreply.github.com>
2020-03-12 19:24:14 +0300

    Add files via upload

---
+++ Laba1/Audi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Audi A4 B6</title>
+    </head>
+
+    <body>
+    	<h1>Обзор семейства автомобилей Ауди А4 универсал Avant</h1>
+    	<p>Универсал Ауди А4 Avant выпускается с момента появления на рынке первого поколения популярной модели немецкой компании. С тех пор как вышла Audi А4 с индексом B5, в модельном ряду неизменно присутствует модификация в кузове универсал Avant. С момента выхода этой модификации в кузове B5 прошло уже больше 20 лет. Сегодня мы рассмотрим все поколения универсала Ауди А4, их характеристики, особенности и слабые места.</p>
+
+		<h2>Универсал Audi А4 B5 Avant</h2>
+		<a href="https://oaudi.ru/wp-content/uploads/2017/04/1-14.jpg" class="linkImgInTextArtAU" target="_blank"><img class="aligncenter wp-image-1395 size-medium" src="https://oaudi.ru/wp-content/uploads/2017/04/1-14-300x192.jpg" alt="Универсал А4 B5 Avant" height="192" width="300" srcset="https://oaudi.ru/wp-content/uploads/2017/04/1-14-300x192.jpg 300w, https://oaudi.ru/wp-content/uploads/2017/04/1-14-768x492.jpg 768w, https://oaudi.ru/wp-content/uploads/2017/04/1-14.jpg 975w" sizes="(max-width: 300px) 100vw, 300px"></a><br>
+		<p>В 1994 году выходит первое поколение Ауди А4 с индексом B5. Сначала был выпущен седан, а примерно через год на рынок выходит модификация универсал Avant. Большой багажник, рейлинги на крыше, пятая дверь ─ вот внешние отличительные особенности универсала A4. Кузов машины был оцинкованный, размер колёсных дисков 15 дюймов.</p>
+
+		<h2>Универсал Audi А4 B6 Avant</h2>
+		<p>Автомобиль Ауди А4 Б6 был показан широкой публике в 2000 году и вскоре после этого был запущен в серийное производство. Первоначально вышел седан, а затем появились версии универсал Avant и кабриолет Cabrio. Модель просуществовала на рынке до 2004 года. Экстерьер универсала Ауди А4 Б6 Avant был похож на второе поколение модели A6 C5. По сравнению с А4 B5 новинка получила более продвинутые системы безопасности. <strong>Секция салона была выполнена малодеформируемой, в базовом оснащении были передние подушки безопасности и боковые на задних сиденьях.</strong> Кроме того, автомобиль оснастили усилителем тормозов для экстренной остановки.</p>
+		<a href="https://oaudi.ru/wp-content/uploads/2017/04/4-10.jpg" class="linkImgInTextArtAU" target="_blank"><img aria-describedby="caption-attachment-1403" class="wp-image-1403 size-medium" src="https://oaudi.ru/wp-content/uploads/2017/04/4-10-300x145.jpg" alt="Универсал Audi А4 B6 Avant" height="145" width="300" srcset="https://oaudi.ru/wp-content/uploads/2017/04/4-10-300x145.jpg 300w, https://oaudi.ru/wp-content/uploads/2017/04/4-10-768x372.jpg 768w, https://oaudi.ru/wp-content/uploads/2017/04/4-10.jpg 944w" sizes="(max-width: 300px) 100vw, 300px"></a><br>
+        <br>
+
+    <u><?php echo "Hello World!" ?></u>
+
+    </body>
+</html>
